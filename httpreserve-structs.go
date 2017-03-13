6f434b67eee09b71296e22343d220e602c38ebb4
ross-spencer <all.along.the.watchtower2001@gmail.com>
2017-03-13 23:02:42 +1300

    [feature] Structs for use in the code.

---
+++ httpreserve-structs.go
@@ -0,0 +1,14 @@
+package main
+
+
+type LinkStats struct {
+	FileName 							string
+	Link 									string
+	ResponseCode 						int
+	ResponseText 						string
+	ScreenShot 							string //href to screenshot
+	InternetArchiveLink 				string
+	InternetArchiveResponseCode 	int
+	InternetArchiveResponseText 	string
+	ArchiveNow							bool
+}
