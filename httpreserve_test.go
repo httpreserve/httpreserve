330c0586075444945a7664180d17e597f14b14ad
ross-spencer <all.along.the.watchtower2001@gmail.com>
2017-03-13 21:36:00 +1300

    [feature] Unit testing.

---
+++ httpreserve_test.go
@@ -0,0 +1,14 @@
+package main 
+
+import "testing"
+
+//sample working perma.cc
+//https://perma.cc/T8U2-994F
+//https://perma.cc/48VC-ZS62
+//https://perma.cc/9265-T4NB
+
+//broken perma.cc (should be a 404)
+//https://perma.cc/48VC-ZS61
+
+
+func TestPlaceHolder(t *testing.T) { }
