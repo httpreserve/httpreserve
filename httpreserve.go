5a1724872aa5d48a965080ed307521304aa78090
ross-spencer <allalongthewatchtower2001@gmail.com>
2017-03-15 18:55:06 +1300

    [refactor] Moving code about to be more flexible.

--- httpreserve.go
+++ httpreserve.go
@@ -1,16 +1,12 @@
 package main
 
 import (
-"net/http/httputil"
-		"os"
-      "net/http"
-      "fmt"
-		//"log"
-		//"io/ioutil"
-      "bufio"
-		"net/url"
-		"github.com/pkg/errors"
-		"github.com/dutchcoders/goftp"
+	"os"
+	"net/http"
+	"fmt"
+	"bufio"
+	"net/url"
+	"github.com/pkg/errors"
    )
 
 //ftp: ftp://exponentialdecay.co.uk/
@@ -33,77 +29,6 @@ const HEAD = http.MethodHead
 
 const USERAGENT = "exponentialDK-httpreserve/0.0.0"
 const BYTERANGE = "bytes=0-0"
-
-func handleftp(request string) (LinkStats, error) {
-	//var ftp *goftp.FTP
-
-	// For debug messages: goftp.ConnectDbg("ftp.server.com:21")
-	//"ftp.server.com:21"
-
-	request = request + ":21"
-	request = "ftp.exponentialdecay.co.uk:21"
-
-	fmt.Println(request)
-	//goftp.ConnectDbg(
-	//TODO: parse debug messages...
-	if _, err := goftp.ConnectDbg(request); err != nil {
-		panic(err)
-	}
-
-	var ls LinkStats
-	return ls, nil
-}
-
-func checkError(err error) {
-    if err != nil {
-        fmt.Println("Fatal error ", err.Error())
-        os.Exit(1)
-    }
-}
-
-//proxy help:
-//https://jannewmarch.gitbooks.io/network-programming-with-go-golang-/content/http/proxy_handling.html
-//another example:
-//http://stackoverflow.com/questions/40817784/access-https-via-http-proxy-with-basic-authentication
-func handlehttp(request string, proxflag bool) (LinkStats, error) {
-
-	var ls LinkStats
-	var client = &http.Client{}
-
-	linkurl, err := url.Parse(request)
-	if err != nil {
-		return ls, errors.Wrap(err, "parse request url failed")
-	}
-
-	req, err := http.NewRequest(HEAD, linkurl.String(), nil)
-	if err != nil {
-		return ls, errors.Wrap(err, "request generation failed")
-	}
-   req.Header.Add("User-Agent", USERAGENT)
-   req.Header.Add("Range", BYTERANGE) 
-
-	if proxflag {
-		client, err = returnProxyClient(req)	
-		if err != nil {
-			return ls, errors.Wrap(err, "proxy header creation failed")
-		}
-	} 
-
-	dump, _ := httputil.DumpRequest(req, false)
-	fmt.Println("Request header:")
-	fmt.Fprintln(os.Stdout, string(dump))
-
-	resp, err := client.Do(req)
-	if err != nil {
-		return ls, errors.Wrap(err, "client request failed")
-	}
-
-	ls.ResponseCode = resp.StatusCode
-	ls.ResponseText = http.StatusText(resp.StatusCode)
-	resp.Body.Close()
-
-	return ls, nil
-}
 
 func testConnection (request string) (LinkStats, error) {
 	var ls LinkStats
