61e48450783efbf63accc576c907533bd43e09c9
ross-spencer <all.along.the.watchtower2001@gmail.com>
2017-03-24 01:33:42 +1300

    [ignore] wrapping up for the night.

--- server/servehttpreserve.go
+++ server/servehttpreserve.go
@@ -2,9 +2,25 @@ package main
 
 import (
 	"fmt"
+	"net/url"
 	"net/http"
+	"net/http/httputil"
 )
 
 func handleHttpreserve(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Return server httpreserve analysis here.")
+	fmt.Fprintln(w, "Return server httpreserve analysis here.\n")
+	req, _ := httputil.DumpRequest(r, false)
+	fmt.Fprintf(w, "%s", req)
+
+	switch r.Method {
+	case http.MethodGet:
+		m, _ := url.ParseQuery(r.URL.String())
+		fmt.Fprintf(w, "\n\n%+v\n\n", m)
+		//fmt.Println(m["k"][0])
+		return
+	case http.MethodPost:
+		r.ParseForm()
+		fmt.Fprintln(w, "\n\n" + r.Form.Get("url"))
+		return
+	}
 }
--- server/static/form/index.js
+++ server/static/form/index.js
@@ -1,21 +1,37 @@
 // Simple AJAX function to make our demo page nice and clean
 function httpreserve() {
-    var xmlhttp = new XMLHttpRequest();
 
-    xmlhttp.onreadystatechange = function() {
-        if (xmlhttp.readyState == XMLHttpRequest.DONE ) {
-           if (xmlhttp.status == 200) {
-               document.getElementById("httpreserve-analysis").innerHTML = xmlhttp.responseText;
-           }
-           else if (xmlhttp.status == 400) {
-              alert('There was an error 400');
-           }
-           else {
-               alert('something else other than 200 was returned');
-           }
-        }
-    };
+	var key=""
+	var value=""
 
-    xmlhttp.open("GET", "httpreserve", true);
-    xmlhttp.send();
+	var elements = document.getElementById("abc").elements;
+	var obj ={};
+	for(var i = 0 ; i < elements.length ; i++){
+		var item = elements.item(i);
+		if (item.name == "url") {
+			key = item.name;
+			value = item.value;
+		}
+	}
+
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function() {
+		if (xmlhttp.readyState == XMLHttpRequest.DONE ) {
+			if (xmlhttp.status == 200) {
+				document.getElementById("httpreserve-analysis").innerHTML = xmlhttp.responseText;
+			}
+			else if (xmlhttp.status == 400) {
+				alert('There was an error 400');
+			}
+			else {
+				alert('something else other than 200 was returned');
+			}
+		}
+	};
+
+	//there?name=ferret
+	//xmlhttp.open("POST", "httpreserve", true);
+	//xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+	xmlhttp.open("GET", "httpreserve?url=http://example.com", true);
+	xmlhttp.send(key + "=" + value);
 }
