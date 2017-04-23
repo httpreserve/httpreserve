acc81cb14ce240dcf5f090b894d999e433e8d6b3
ross-spencer <all.along.the.watchtower2001@gmail.com>
2017-04-23 20:36:37 +1200

    [testing] tests.

--- httpreserve_test.go
+++ httpreserve_test.go
@@ -1,24 +1,8 @@
 package httpreserve
 
 import (
-	"fmt"
 	"testing"
 )
-
-func TestIALinkNowDate(t *testing.T) {
-	archiveurl, _ := GetPotentialURLLatest("http://www.bbc.co.uk/news")
-	fmt.Println(archiveurl)
-}
-
-func TestIALinkEarliestDate(t *testing.T) {
-	archiveurl, _ := GetPotentialURLEarliest("http://www.bbc.co.uk/news")
-	fmt.Println(archiveurl)
-}
-
-func TestSavedURL(t *testing.T) {
-	u, _ := GetSavedURL(generateInternetArchiveSaveMock())
-	fmt.Println(u)
-}
 
 func TestPlaceHolder(t *testing.T) {
 
