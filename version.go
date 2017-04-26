6cd1f05ebb62cb473b60f49412017d89442cb530
ross-spencer <all.along.the.watchtower2001@gmail.com>
2017-04-27 00:13:15 +1200

    [feature] Version 0.0.1. All features implemented one way or another.

--- version.go
+++ version.go
@@ -6,7 +6,7 @@ const httpUSERAGENT = "exponentialDK-httpreserve/"
 
 // Version will return a simple version number for the app.
 func Version() string {
-	return "0.0.0"
+	return "0.0.1"
 }
 
 // VersionNumber is a synonym for Version()
