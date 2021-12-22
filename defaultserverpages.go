c0f7073099e19939fae01f01f7badac7d49e0899
Ross Spencer <all.along.the.watchtower2001@gmail.com>
2021-12-22 16:50:24 +0100

    Update the fonts used on the figure element
    Fonts should have been sans-serif but the CSS has not been working
    for <figure> and it isn't immediately obviously why. We go back to
    using the style attribute to force the style we want.

--- defaultserverpages.go
+++ defaultserverpages.go
@@ -41,160 +41,160 @@ func GetDefaultServerPage(method string) string {
 
 // Web entry point for our default server for demo purposes.
 var httpreservePages = `
-	<!DOCTYPE html>
-	<html lang="en">
-	<head>
-		<meta charset="utf-8">
-		<title>httpreserve</title>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>httpreserve</title>
 
-		<link id="favicon" rel="icon" type="image/x-icon"
-		href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAABm
-		JLR0QA/wD/AP+gvaeTAAAACXBIWXMAAAsTAAALEwEAmpwYAAAA
-		B3RJTUUH4QMZAiAVgKAUZQAAAGlJREFUWMPt1jkSgDAMQ9GYk/
-		vmoqIM4C1qpAPkv5k0NgBYxF2LPAE0rTQADsCZ8WfOjJ9FbOJn
-		EB/xWcTP+AwiGO9FJOM9iGK8hmiK5xDN8RhiKL5FWOJrXm9IMw
-		u9qZNMAAEEEIAOuAEmTWnhcv1r2AAAAABJRU5ErkJggg=="/>
+        <link id="favicon" rel="icon" type="image/x-icon"
+        href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAABm
+        JLR0QA/wD/AP+gvaeTAAAACXBIWXMAAAsTAAALEwEAmpwYAAAA
+        B3RJTUUH4QMZAiAVgKAUZQAAAGlJREFUWMPt1jkSgDAMQ9GYk/
+        vmoqIM4C1qpAPkv5k0NgBYxF2LPAE0rTQADsCZ8WfOjJ9FbOJn
+        EB/xWcTP+AwiGO9FJOM9iGK8hmiK5xDN8RhiKL5FWOJrXm9IMw
+        u9qZNMAAEEEIAOuAEmTWnhcv1r2AAAAABJRU5ErkJggg=="/>
 
-		<script type="text/javascript">
-			// Simple AJAX function to make our demo page nice and clean
-			function httpreserve() {
+        <script type="text/javascript">
+            // Simple AJAX function to make our demo page nice and clean
+            function httpreserve() {
 
-				var myform = document.getElementById("httpreserve-form");
+                var myform = document.getElementById("httpreserve-form");
 
-				var key="";
-				var value="";
+                var key="";
+                var value="";
 
-				var method = myform.method;
-				var elements = myform.elements;
+                var method = myform.method;
+                var elements = myform.elements;
 
-				var obj ={};
-				for(var i = 0 ; i < elements.length ; i++){
-					var item = elements.item(i);
-					if (item.name == "url") {
-						key = item.name;
-						value = item.value;
-					}
-				}
+                var obj ={};
+                for(var i = 0 ; i < elements.length ; i++){
+                    var item = elements.item(i);
+                    if (item.name == "url") {
+                        key = item.name;
+                        value = item.value;
+                    }
+                }
 
-				// Add HTTP:// by default if not in string
-				value = addhttp(value)
+                // Add HTTP:// by default if not in string
+                value = addhttp(value)
 
-				var xmlhttp = new XMLHttpRequest();
-				xmlhttp.onreadystatechange = function() {
-					if (xmlhttp.readyState == XMLHttpRequest.DONE ) {
+                var xmlhttp = new XMLHttpRequest();
+                xmlhttp.onreadystatechange = function() {
+                    if (xmlhttp.readyState == XMLHttpRequest.DONE ) {
 
-						document.getElementById("loader").innerHTML = "";
+                        document.getElementById("loader").innerHTML = "";
 
-						if (xmlhttp.status == 200) {
-							document.getElementById("httpreserve-analysis").innerHTML = xmlhttp.responseText;
-						}
-						else if (xmlhttp.status == 400) {
-						   document.getElementById("httpreserve-analysis").innerHTML = '[WARNING] There was an error 400';
-						}
-						else {
-						   document.getElementById("httpreserve-analysis").innerHTML = '[WARNING] something else other than 200 was returned';
-						}
-					} else {
-						document.getElementById("httpreserve-analysis").innerHTML = "";
-						document.getElementById("loader").innerHTML = {{ LOADING }};
-					}
-				};
+                        if (xmlhttp.status == 200) {
+                            document.getElementById("httpreserve-analysis").innerHTML = xmlhttp.responseText;
+                        }
+                        else if (xmlhttp.status == 400) {
+                           document.getElementById("httpreserve-analysis").innerHTML = '[WARNING] There was an error 400';
+                        }
+                        else {
+                           document.getElementById("httpreserve-analysis").innerHTML = '[WARNING] something else other than 200 was returned';
+                        }
+                    } else {
+                        document.getElementById("httpreserve-analysis").innerHTML = "";
+                        document.getElementById("loader").innerHTML = {{ LOADING }};
+                    }
+                };
 
-				if (method.toLowerCase() == "post") {
-					xmlhttp.open("POST", "httpreserve", true);
-					xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-					xmlhttp.send(key + "=" + value);
-					return;
-				}
+                if (method.toLowerCase() == "post") {
+                    xmlhttp.open("POST", "httpreserve", true);
+                    xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+                    xmlhttp.send(key + "=" + value);
+                    return;
+                }
 
-				if (method.toLowerCase() == "get") {
-					xmlhttp.open("GET", "httpreserve?" + key + "=" + value, true);
-					xmlhttp.send(key + "=" + value);
-					return;
-				}
+                if (method.toLowerCase() == "get") {
+                    xmlhttp.open("GET", "httpreserve?" + key + "=" + value, true);
+                    xmlhttp.send(key + "=" + value);
+                    return;
+                }
 
-				document.getElementById("httpreserve-analysis").innerHTML = '[WARNING] issue parsing the form in JavaScript';
-			}
+                document.getElementById("httpreserve-analysis").innerHTML = '[WARNING] issue parsing the form in JavaScript';
+            }
 
-			// Search for HTTP:// in our string for usability of tool
-			// will fail if we haven't a protocol.
-			function addhttp(value) {
-				if (value.indexOf("http://") != -1) {
-					return value
-				}
-				if (value.indexOf("https://") != -1) {
-					return value
-				}
-				if (value.indexOf("ftp://") != -1) {
-					return value
-				}
-				var newurl = "http://".concat(value)
-				return newurl
-			}
+            // Search for HTTP:// in our string for usability of tool
+            // will fail if we haven't a protocol.
+            function addhttp(value) {
+                if (value.indexOf("http://") != -1) {
+                    return value
+                }
+                if (value.indexOf("https://") != -1) {
+                    return value
+                }
+                if (value.indexOf("ftp://") != -1) {
+                    return value
+                }
+                var newurl = "http://".concat(value)
+                return newurl
+            }
 
-		</script>
-		<style>
-			body { min-width:750px; font-family: arial, verdana; font-size: 10px; margin-bottom: 20px}
-			figcaption { font-family: times new roman, arial, verdana; font-size: 20px; font-weight: bold; margin-bottom: 8px; }
+        </script>
+        <style>
+            body { min-width:750px; font-family: arial, verdana; font-size: 10px; margin-bottom: 20px}
+            figure { font-family: times new roman, arial, verdana; font-size: 20px; font-weight: bold; margin-bottom: 8px; }
 
-			figcaption.loading { font-family: arial, verdana; font-size: 8px; margin-top: -2px; font-weight: normal; }
+            figure.loading { font-family: arial, verdana; font-size: 8px; margin-top: -2px; font-weight: normal; }
 
-			div.wrap { margin: 0 auto ; width:715px; }
-			div.layout { margin-top: 50px; min-height: 100%; height: 250px; }
+            div.wrap { margin: 0 auto ; width:715px; }
+            div.layout { margin-top: 50px; min-height: 100%; height: 250px; }
 
-			h4 { font-size: 16px; margin-bottom: -2px;}
+            h4 { font-size: 16px; margin-bottom: -2px;}
 
-			input.link { display: block; margin: auto; width: 500px; }
-			input.button  { display: block; margin: auto; }
+            input.link { display: block; margin: auto; width: 500px; }
+            input.button  { display: block; margin: auto; }
 
          /*use push to position footer more usefully on screen if necessary*/
          div.push { height: 340px; min-height: 340px; }
 
          div.footer { height: 50px; margin: 0 auto ; width:200px; text-align: center; }
-		</style>
-	</head>
-	<body>
-	<div class="wrap">
-		<div class="layout">
-		<p>
-		<center>
-			<figure>
-			  	<figcaption>httpreserve</figcaption>
-				<img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmc
-				vMjAwMC9zdmciIHdpZHRoPSI4IiBoZWlnaHQ9IjgiIHZpZXdCb3g9IjAgMCA4IDgiPg0KIC
-				A8cGF0aCBkPSJNMCAwdjFoOHYtMWgtOHptNCAybC0zIDNoMnYzaDJ2LTNoMmwtMy0zeiIgLz4NCjwvc3ZnPg=="
-				width="50px" height="50px" alt="httpreserve"/>
-			</figure>
-		</center>
-		</p>
-		<h4>Enter a URL:</h4>
-		<p>
-		This service will tell you if an internet archive link exists and give you a mechanism to save it as you wish.
-		<br/>
-		<br/>
-		<form action="javascript:httpreserve()" ` + templateFormMethod + ` id="httpreserve-form">
-		   <input class="link" type="text" name="url" value="http://example.com/">
-		   <br/>
-		   <input class="button" type="submit" value="httpreserve">
-		</form>
-		<br/><br/>
-		<pre id="httpreserve-analysis"></div>
-		</p>
-		<div>
-		<center>
-		<div id="loader"></div>
-		</center>
-		</div>
-		<div class="push">&nbsp;</div>
-		</div>
+        </style>
+    </head>
+    <body>
+    <div class="wrap">
+        <div class="layout">
+        <p>
+        <center>
+            <figure>
+                <figcaption style="font-family: helvetica; arial, verdana; margin-bottom: 8px">httpreserve</figcaption>
+                <img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmc
+                vMjAwMC9zdmciIHdpZHRoPSI4IiBoZWlnaHQ9IjgiIHZpZXdCb3g9IjAgMCA4IDgiPg0KIC
+                A8cGF0aCBkPSJNMCAwdjFoOHYtMWgtOHptNCAybC0zIDNoMnYzaDJ2LTNoMmwtMy0zeiIgLz4NCjwvc3ZnPg=="
+                width="50px" height="50px" alt="httpreserve"/>
+            </figure>
+        </center>
+        </p>
+        <h4>Enter a URL:</h4>
+        <p>
+        This service will tell you if an internet archive link exists and give you a mechanism to save it as you wish.
+        <br/>
+        <br/>
+        <form action="javascript:httpreserve()" ` + templateFormMethod + ` id="httpreserve-form">
+           <input class="link" type="text" name="url" value="http://example.com/">
+           <br/>
+           <input class="button" type="submit" value="httpreserve">
+        </form>
+        <br/><br/>
+        <pre id="httpreserve-analysis"></div>
+        </p>
+        <div>
+        <center>
+        <div id="loader"></div>
+        </center>
+        </div>
+        <div class="push">&nbsp;</div>
+        </div>
       <div class="footer" id="footer">
-		A project by <a href="https://twitter.com/beet_keeper" alt="@beet_keeper on Twitter">@beet_keeper</a>
-		<br/>
-		On GitHub: <a href="https://github.com/exponential-decay/httpreserve" alt="httpreserve on GitHub">httpreserve</a>
-	</div>
-	</div>
-	</body>
-	</html>
+        A project by <a href="https://twitter.com/beet_keeper" alt="@beet_keeper on Twitter">@beet_keeper</a>
+        <br/>
+        On GitHub: <a href="https://github.com/exponential-decay/httpreserve" alt="httpreserve on GitHub">httpreserve</a>
+    </div>
+    </div>
+    </body>
+    </html>
 
 `
 
@@ -720,5 +720,5 @@ AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
 AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
 AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
 AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=" width="30px" height="30px" alt="loading"/>
-<figcaption class="loading">processing</figcaption></figure>'
+<figcaption class="loading" style="font-family: helvetica; arial, verdana;">processing</figcaption></figure>'
 `
