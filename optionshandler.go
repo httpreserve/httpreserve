e268a9555b3a609fecb5e908904bb01b008c93a9
Ross Spencer <all.along.the.watchtower2001@gmail.com>
2021-12-22 17:48:11 +0100

    Update the OPTIONS instructions

--- optionshandler.go
+++ optionshandler.go
@@ -17,53 +17,49 @@ _  __________ _____________________  _____
 
 `
 
-var byline = "A service to help you audit the state of your\nweblinks."
+var byline = "A service to help you audit the state of your weblinks."
 
 var instructions = `
-There are two things you can do with this service right 
-now these are documented below.
+There are two things you can do with this service, these are documented below.
 
 See the status of your weblink:
 
-   curl -i -X GET http://httpreserve.info/httpreserve?
-                   url=http://www.example.com
-                   &filename=example.txt
+   curl -i -X GET http://httpreserve.info/httpreserve?url=http://www.example.com
 
-Manage the transaction with the wayback machine 
-to save your link:
+Manage the transaction with the wayback machine to save your link:
 
-   curl -i -X GET http://httpreserve.info/save?
-                   url=http://www.example.com
-                   &filename=example.txt
+   curl -i -X GET http://httpreserve.info/save?url=http://www.example.com
 
 POST will also work if you encode your form:
 
    application/x-www-form-urlencoded
 
-Filename is encoded in the URL to help folks audit their 
-repositories. It is expected they will extract the links
-contained deep in their documentary heritage, word docs,
-pdf, wordperfect, etc. using a tool like tikalinkextractor
+Filename is encoded in the URL to help folks audit their repositories.
+It is expected they will extract the links contained deep in their documentary
+heritage, word docs, pdf, wordperfect, etc. using a tool like tikalinkextractor
 
    https://github.com/httpreserve/tikalinkextract
 
-And then run it through httpreserve to report on the links
-still standing since the advent of the web and the 
-intersection with the paper-paradigm for office productivity.
+And then run it through httpreserve to report on the links still standing since
+the advent of the web and the intersection with the paper-paradigm for office
+productivity.
 
-httpreserve.info is a demo server running on the cheapest
-VPS option available at Linode.com which means it's really
-just a demo for now but feel free to wrap it into your 
-scripts. 
+httpreserve.info is a demo server running on the cheapest VPS option available
+at Linode.com which means it's really just a demo for now but feel free to wrap
+it into your scripts.
 
-See workbench app for a more powerful way to use this tool
-with bulk lists of links. 
+See workbench app for a more powerful theoretical way to use this tool with
+bulk lists of links.
 
    https://github.com/httpreserve/workbench
 
+A more practical implementation is at:
+
+   https://github.com/httpreserve/linkstat
+
 The full suite of tools can be found at httpreserve suite.
 
-   https://github.com/httpreserve
+   https://github.com/httpreserve/
 `
 
 func handleOptions(w http.ResponseWriter, r *http.Request) {
