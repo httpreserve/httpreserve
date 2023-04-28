f4a34de41f5c373e554cdf6987a58296651e28a1
Ross Spencer <all.along.the.watchtower2001@gmail.com>
2023-04-28 13:27:43 +0200

    Up version 0.0.13

--- version.go
+++ version.go
@@ -6,7 +6,7 @@ const httpUSERAGENT = "exponentialDK-httpreserve/"
 
 // Version will return a simple version number for the app.
 func Version() string {
-	return "0.0.12"
+	return "0.0.13"
 }
 
 // VersionNumber is a synonym for Version()
